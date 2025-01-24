91607224dbbb948f1cde8e3fa094601f047bfe25
TMarcus68 <tmarcus68@gmail.com>
2025-01-25 01:32:35 +0800

    add qingyou

---
+++ src/app/qingyou/page.tsx
@@ -0,0 +1,11 @@
+"use client"; // This directive makes this component a Client Component
+
+import MapBoxWidgetFull from "../components/MapBoxWidgetFull";
+
+export default function Qingyou() {
+  return (
+    <main className="container">
+      <MapBoxWidgetFull />
+    </main>
+  );
+}
